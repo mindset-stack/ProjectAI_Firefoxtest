74cd707099a8cefe4884563f20f5f92c083f277b
vijay.sankpal <vijay.sankpal@blazelcan.com>
2023-03-17 16:20:26 +0530

    y

--- UndestandingDriverFlow/src/main/java/com/blazeclan/qa/utility/CrossBrowser.java
+++ UndestandingDriverFlow/src/main/java/com/blazeclan/qa/utility/CrossBrowser.java
@@ -3,53 +3,34 @@ package com.blazeclan.qa.utility;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
 import com.blazeclan.qa.base.CommonFunctions;
 import com.blazeclan.qa.logging.Log;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class CrossBrowser extends CommonFunctions{
+public class CrossBrowser extends CommonFunctions {
 
 	
 	public static WebDriver selectDriver(String browser) {
-		
-		switch(browser){
-		case"chrome":
-		ChromeOptions options=  new ChromeOptions();
-		options.addArguments("--start-maximized");
-		options.addArguments("--remote-allow-origins=*");
-		//options.addArguments("--remote-allow-origins=*","ignore-certificate-errors");
-		
-		System.setProperty("webdriver.http.factory", "jdk-http-client");
-		//options.addArguments("no-sandbox");
-		
-		
-		WebDriverManager.chromedriver().setup();
-       driver = new ChromeDriver(); 
-	//driver= WebDriverManager.chromedriver().capabilities(options).create();
-		
-		
 
-			
+		if (browser.equalsIgnoreCase("Chrome")) {
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+		} else if (browser.equalsIgnoreCase("FireFox")) {
+			WebDriverManager.firefoxdriver().setup();
+			driver = new FirefoxDriver();
+		} else {
+
+		}
+
 		Log.info("chrome browser selected");
-		break;
-	default:
-		System.out.println("Please provide browser name");
-	//System.setProperty("webdriver.chrome.driver", "D://Latestchromedriverexe//chromedriver.exe");
-		//System.setProperty("webdriver.chrome.driver", "/usr/bin/google-chrome");
-		
-		
-		WebDriver driver = new ChromeDriver();
-		driver.manage().window().maximize();
-		
-	}
+
 		DriverManager.setDriver(driver);
 		return DriverManager.getDriver();
-	
-	
-		
-		
+
 	}
-	
-	
 }
